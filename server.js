04c50cb06366d25ed37bcd90c178549f33db27dd
Alvaro Blanco <alvarofblanco@gmail.com>
2020-09-15 21:04:31 -0400

    changed message

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3333
 
 app.get('/', async (req, res) => {
-  res.send('Hello world')
+  res.send('Hola desde el contenedor')
 })
 
 app.listen(PORT, () => {
